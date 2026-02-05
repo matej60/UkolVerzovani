8d5b25c45f9a2247a67d979afcb46a78640d766e
Matej Fiser <matejf03@gmail.com>
2026-02-05 09:48:36 +0100

    vytvoření funkce pro vyzkoušení LinQ syntaxe

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            linQTest();
+
+            Console.ReadKey();
+        }
+        public static void linQTest()
+        {
+            List<int> list = new List<int>() { 15, 2, 35, -1, 0, 20, 9, 17, 5, 4, -7 };
+            IEnumerable<int> listOut =
+                from num in list
+                where num > 0 && num < 10
+                select num;
+            Console.WriteLine(string.Join(",", listOut) + " " + listOut.Count());
+
+            var listOutLinQ = list.Where(x => x > 0 && x < 10);
+
+            Console.WriteLine(string.Join(",", listOutLinQ) + " " + listOutLinQ.Count());
+
+            IEnumerable<int> sortedInts = list.OrderBy(x => x);
+
+            foreach (int i in sortedInts)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.Write("\n" + string.Join(", ", Enumerable.Range(1, 30)));
+            Console.Write("\n" + string.Join(", ", sortedInts));
+
         }
     }
 }
